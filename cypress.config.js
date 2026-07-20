7590a22faad82ab1bdd8e01e39617e7cfd02c4bf
Lincoln Venancio <lincolnvenancio@gmail.com>
2026-07-20 05:57:44 -0300

    chore: configure Cypress project

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://front.serverest.dev',
+
+    env: {
+      apiUrl: 'https://serverest.dev',
+    },
+
+    setupNodeEvents(on, config) {
+      // Node events
+    },
+  },
+});
